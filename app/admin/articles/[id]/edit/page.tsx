3f6a889486c3c58b5f19d0a8d4967ec691d3cde9
DanielPierre2023 <152337490+DanielPierre2023@users.noreply.github.com>
2026-03-24 00:23:21 +0100

    Create page.tsx

---
+++ app/admin/articles/[id]/edit/page.tsx
@@ -0,0 +1,5 @@
+import ArticleEditor from '@/app/admin/components/ArticleEditor'
+export default async function EditArticlePage({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params
+  return <ArticleEditor articleId={id} />
+}
