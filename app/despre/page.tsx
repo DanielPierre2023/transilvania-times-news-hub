effdbf5c5b975bdadaab96797dfbf8ea31519e04
DanielPierre2023 <152337490+DanielPierre2023@users.noreply.github.com>
2026-05-26 09:56:02 +0200

    Disable ESLint rule for unescaped entities

--- app/despre/page.tsx
+++ app/despre/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import { createSupabaseServerClient } from '@/lib/supabase/server'
 import Link from 'next/link'
 import type { Metadata } from 'next'
